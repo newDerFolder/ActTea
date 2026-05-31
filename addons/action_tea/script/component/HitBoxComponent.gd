4a2b8dbb92b38dc154e04df533562cfb9cb82ce0
newDerFolder <KLderMR@163.com>
2026-05-31 20:39:44 +0800

    hit

---
+++ addons/action_tea/script/component/HitBoxComponent.gd
@@ -0,0 +1,5 @@
+class_name HitBoxComponent extends Area2D
+
+@export var damage: int = 10
+
+signal hit(hurtbox: HurtboxComponent)
--- addons/action_tea/script/component/HurtboxComponent.gd
+++ addons/action_tea/script/component/HurtboxComponent.gd
@@ -1,1 +1,15 @@
 class_name HurtboxComponent extends Area2D
+
+@export var health_component:HealthComponent
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	pass
+
+
+func _on_area_entered(area: Area2D):
+	if not area is HitBoxComponent:
+		return
+	var damage=(area as HitBoxComponent).damage
+	health_component.take_damage(damage)
+	area.hit.emit(self)
