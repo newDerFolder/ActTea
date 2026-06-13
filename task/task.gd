4587300e587e4142ecb35eff9ada9004ac5e02fc
newDerFolder <KLderMR@163.com>
2026-06-13 18:14:03 +0800

    dev

--- task/task.gd
+++ task/task.gd
@@ -13,7 +13,8 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	$CharacterBody2D.position.x-=100*delta
-
+	if $HurtboxComponent/StateGroupNode.get_now_state_name()=="move":
+		$HurtboxComponent.position.y+=50*delta
 
 func _on_hit_box_component_hit(hurtbox: HurtboxComponent) -> void:
 	print("打中了")
@@ -25,3 +26,5 @@ func _on_health_component_on_damaged(damage_amount: int) -> void:
 
 func _on_enemy_radar_component_enemy_detected(area: HurtboxComponent) -> void:
 	print("扫描到敌人了",area.global_position)
+	if $HurtboxComponent/StateGroupNode.get_now_state_name()=="await":
+		$HurtboxComponent/StateGroupNode/awaitt.transition_by_index(0)
