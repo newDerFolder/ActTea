a46d95e20aa46279354c002f808f6d08544d4b74
newDerFolder <KLderMR@163.com>
2026-06-16 08:29:09 +0800

    set team

--- addons/action_tea/script/component/BaseGameCharacter2D.gd
+++ addons/action_tea/script/component/BaseGameCharacter2D.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 
 
 func set_team(team:TeamMemberComponent.Team):
-	team=team
+	self.team=team
 	for i in get_children():
 		if i is TeamMemberComponent:
 			i.character=self
