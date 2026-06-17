24f452f6e6e9d2123470e8b82012278678106780
newDerFolder <KLderMR@163.com>
2026-06-17 14:47:29 +0800

    hp bar

---
+++ addons/action_tea/script/component/health/HealthBarComponent.gd
@@ -0,0 +1,16 @@
+class_name HealthBarComponent extends ProgressBar
+
+
+@export var health_component:HealthComponent
+
+func _ready() -> void:
+	if health_component==null:
+		push_error("HealthBarComponent没有绑定HealthComponent")
+		return
+	else:
+		health_component.on_health_changed.connect(_on_health_component_health_changed)
+
+
+func _on_health_component_health_changed(hp:int,max_hp:int):
+	max_value=max_hp
+	value=hp
